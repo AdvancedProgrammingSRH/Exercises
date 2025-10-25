da56baeda763fc078b9a4b7c80893427165f28e6
EstebanPozo <62659091+EstebanPozo@users.noreply.github.com>
2025-10-25 12:37:05 +0200

    compare with 0.3

---
+++ 2_DataTypes_FloatingPoint.py
@@ -0,0 +1,8 @@
+value1 = 0.1
+value2 = 0.2
+total = value1 + value2
+print("The total is:", total)
+if total == 0.3:
+    print("The total is exactly 0.3")
+else:
+    print("The total is not exactly 0.3")
