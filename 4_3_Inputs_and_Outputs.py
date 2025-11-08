e184273cecf887cfaa8f04a8c9d9b9d1a82218ec
EstebanPozo <62659091+EstebanPozo@users.noreply.github.com>
2025-11-08 11:43:34 +0100

    monetary value

--- 4_3_Inputs_and_Outputs.py
+++ 4_3_Inputs_and_Outputs.py
@@ -1,3 +1,6 @@
 value_str  = input("Enter a monetary vakue: ")
+=======
+value_str  = input("Enter a monetary value: ")
+>>>>>>> Stashed changes
 value_float = float(value_str)
 print(f"{value_str} as a float is ${value_float:.2f}")
