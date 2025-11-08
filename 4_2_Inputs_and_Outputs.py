7113e00e0777c4e6e71763e1ff849d4a16e51bb9
EstebanPozo <62659091+EstebanPozo@users.noreply.github.com>
2025-11-08 09:46:11 +0100

    monetary value

---
+++ 4_2_Inputs_and_Outputs.py
@@ -0,0 +1,7 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")   
+
+sum_result = float(num1) + float(num2)
+Product = float(num1) * float(num2)
+print("The sum of " + num1 + " and " + num2 + " is: " + str(sum_result))
+print("Sum", sum_result, "| Product", Product, sep=" --> ")
